49e5de56f9347279694fe9cbe88013c89c66ed63
Lilian Geovana Garcia Saraiva Botelho <lilian.botelho@rr.sebrae.com.br>
2024-01-23 10:44:44 -0300

    css v1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '0px 8px 32px rgba(17, 17, 26, 0.05)',
+      }
+    },
   },
   plugins: [],
 }
