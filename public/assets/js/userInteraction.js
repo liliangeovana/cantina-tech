b820686f122f897616460169cf88f82bef014abe
Lílian Saraiva <lili.ggsb@gmail.com>
2024-01-23 18:43:15 -0400

    pagina de cadastro

--- public/assets/js/userInteraction.js
+++ public/assets/js/userInteraction.js
@@ -10,31 +10,11 @@ var returnLink = document.getElementById('returnLink');
 
 //BOTÃO CADASTRE-SE
 document.addEventListener("DOMContentLoaded", function () {
-    const savedScreen = localStorage.getItem("currentScreen");
-
-    if (savedScreen === "signUpScreen") {
-        showSignUpScreen();
-    }
-
-    document.getElementById("signUpScreenButton").addEventListener("click", function () {
-        localStorage.setItem("currentScreen", "signUpScreen");
-        showSignUpScreen();
-    });
 
     document.getElementById("backButton").addEventListener("click", function () {
-        localStorage.removeItem("currentScreen");
         showLoginScreen();
     });
 
-    function showLoginScreen() {
-        document.getElementById("loginScreen").classList.remove("hidden");
-        document.getElementById("signUpScreen").classList.add("hidden");
-    }
-
-    function showSignUpScreen() {
-        document.getElementById("loginScreen").classList.add("hidden");
-        document.getElementById("signUpScreen").classList.remove("hidden");
-    }
 
 
 //CAMPOS TELA DE LOGIN
