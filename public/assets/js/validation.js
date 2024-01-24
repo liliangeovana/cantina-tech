8b6ea8077f74506012459bdcbb88be2300c2b661
Lilian Geovana Garcia Saraiva Botelho <lilian.botelho@rr.sebrae.com.br>
2024-01-24 12:56:48 -0300

    ajuste tailwind

--- public/assets/js/validation.js
+++ public/assets/js/validation.js
@@ -5,3 +5,17 @@ var email = document.getElementById('signUpEmail');
 var password = document.getElementById('singUpPassword');
 var confirmPassword = document.getElementById('confirmPassword');
 
+//Campo nome
+userName.addEventListener('input', function () {
+    var inputValue = userName.value;
+   // Verificar se o valor contém apenas letras usando uma expressão regular
+    if (/[^a-zA-Z]/.test(inputValue)) {
+        // Se contiver números, aplicar borda vermelha
+        userName.classList.remove('focus:border-blue-900');
+        userName.classList.add('focus:border-red-400');
+    } else {
+        // Se contiver apenas letras, remover a borda vermelha
+        userName.classList.remove('focus:border-red-400');
+        userName.classList.add('focus:border-blue-900');
+    }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  mode: 'jit',
+  content: ["./**/*.html", "./**/*.js"],
   theme: {
-    extend: {
-      boxShadow: {
-        '3xl': '0px 8px 32px rgba(17, 17, 26, 0.05)',
-      }
-    },
   },
   plugins: [],
 }
