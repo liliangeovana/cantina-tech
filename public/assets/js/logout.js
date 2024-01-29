c3175ef72e99dfdf89330c1f10f4ea65a57d6c1e
Lilian Geovana Garcia Saraiva Botelho <lilian.botelho@rr.sebrae.com.br>
2024-01-29 11:36:17 -0300

    home mobile ok

--- public/assets/js/logout.js
+++ public/assets/js/logout.js
@@ -1,4 +1,4 @@
-function confirmLogout() {
+document.getElementById('logOutButton').addEventListener('click', function() {
     var userDecision = confirm("Deseja limpar o localStorage ao sair?");
 
     if (userDecision) {
@@ -10,4 +10,11 @@ function confirmLogout() {
         alert("Você escolheu não limpar o localStorage. Você será redirecionado para a página de login.");
         window.location.href = "../../index.html";
     }
+})
+
+if (localStorage.getItem('name')) {
+    // Se o nome do usuário existir, atualiza o conteúdo do elemento span
+    document.getElementById('userIdentification').textContent = localStorage.getItem('name');
+} else {
+    document.getElementById('userIdentification').textContent = 'user';
 }
